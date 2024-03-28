ba44ce1fb634980529022557f2a369e8dc8d234e
sebanimm <gks32114221@gmail.com>
2024-03-29 00:33:14 +0900

    feat: 시간 입력 기능 추가

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class App(Frame):
     mp3_path = "assets/kuru_kuru_herta.mp3"
 
     def __init__(self, root):
-        super().__init__(root, bg="WHITE")
+        super().__init__(root)
 
         self.main_frame = self
         self.main_frame.pack(fill=BOTH, expand=True)
@@ -22,30 +22,50 @@ class App(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.label_gif1 = Label(
+        self.gif_label = Label(
             self.main_frame, bg="BLACK", border=0, highlightthickness=0
         )
 
-        self.label_gif1.grid(column=0, row=0)
+        self.gif_label.grid(column=0, row=0)
 
-        self.gif1_frames = self._get_frames(self.gif_path)
+        self.gif_frames = self._get_frames(self.gif_path)
 
-        root.after(0, self._play_gif, self.label_gif1, self.gif1_frames)
+        root.after(0, self._play_gif, self.gif_label, self.gif_frames)
 
-        self.button = Button(
-            self.main_frame,
+        self.config_frame = Frame(self.main_frame)
+
+        self.entry_frame = LabelFrame(self.config_frame, text="타이머 주기 설정")
+
+        self.minutes = IntVar(value=10)
+
+        self.minutes_entry = Entry(self.entry_frame, width=4, textvariable=self.minutes)
+        self.minutes_entry.pack(side=LEFT)
+
+        self.minutes_label = Label(self.entry_frame, text="분", width=3)
+        self.minutes_label.pack(side=LEFT)
+
+        self.seconds = IntVar()
+
+        self.seconds_entry = Entry(self.entry_frame, width=4, textvariable=self.seconds)
+        self.seconds_entry.pack(side=LEFT)
+
+        self.seconds_label = Label(self.entry_frame, text="초", width=3)
+        self.seconds_label.pack(side=LEFT)
+
+        self.entry_frame.pack(side=LEFT)
+
+        self.start_button = Button(
+            self.config_frame,
             text="시작",
             width=10,
             height=2,
-            command=self._repeat_play_mp3,
+            command=self._repeat_mp3,
         )
+        self.start_button.pack(side=RIGHT)
 
-        self.button.grid(column=0, row=1)
+        self.config_frame.grid(column=0, row=1, sticky=W + E, padx=50)
 
-        def _stop_program():
-            root.destroy()
-
-        root.protocol("WM_DELETE_WINDOW", _stop_program)
+        root.protocol("WM_DELETE_WINDOW", self._stop_program)
 
     def _get_frames(self, img):
         with Image.open(img) as gif:
@@ -80,10 +100,16 @@ class App(Frame):
 
         label.config(image=frame)
 
-    def _repeat_play_mp3(self):
+    def _repeat_mp3(self):
+        timer_minutes_cycle = self.minutes.get()
+        timer_seconds_cycle = self.seconds.get()
+        timer_cycle_into_seconds = timer_minutes_cycle * 60 + timer_seconds_cycle
+
         playsound(self.mp3_path, block=False)
 
-        schedule.every(10).minutes.do(playsound, self.mp3_path)
+        schedule.every(timer_cycle_into_seconds).seconds.do(
+            playsound, self.mp3_path, block=False
+        )
 
         def start_schedule():
             while True:
@@ -92,6 +118,9 @@ class App(Frame):
 
         schedule_thread = threading.Thread(target=start_schedule)
         schedule_thread.start()
+
+    def _stop_program(self):
+        root.destroy()
 
 
 root = Tk()
